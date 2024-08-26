7406f2222c733cf80ffb9eba8211bc2e0848d08c
stephen ikani <steville2013@gmail.com>
2024-08-26 14:01:56 +0100

    add a dto package with purchase and purchaseresponse classes with their corresponding getters and setters

---
+++ src/main/java/com/tastefultreasure/ecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,16 @@
+package com.tastefultreasure.ecommerce.dto;
+
+public class PurchaseResponse {
+	private String orderTrackingNumber;
+	
+	// Getter and Setter
+
+    public String getOrderTrackingNumber() {
+        return orderTrackingNumber;
+    }
+
+    public void setOrderTrackingNumber(String orderTrackingNumber) {
+        this.orderTrackingNumber = orderTrackingNumber;
+    }
+
+}
